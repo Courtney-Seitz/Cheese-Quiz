7491402a21f1d1ff5dbdb59060dfa8cf9df6f364
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2020-04-02 22:23:01 -0400

    moar

--- store.js
+++ store.js
@@ -1,14 +1,14 @@
 const STORE = {
   questions: [
     {
-      question: "",
+      question: "What’s the difference between making cheese and yogurt?",
       options: [
-        "",
-        "",
-        "",
-        ""
+        "Milk for yogurt is initially heated to 185 deg for 30 min.",
+        "Only one is produced using starter cultures.",
+        "For the cheese the milk thickens, for yogurt curds are formed.",
+        "With yogurt, effort is made to remove the whey."
       ],
-      answer: ""
+      answer: "Milk for yogurt is initially heated to 185 deg for 30 min."
     },
     {
       question: "",
